e3bab906b5c1f589e337cac23b9ee7cd3bd665e0
vykrum <testingwaters@gmail.com>
2024-02-18 14:40:52 +0530

    Annotate Main

--- Main.fs
+++ Main.fs
@@ -7,14 +7,14 @@ open Bridge
 
 type Model =
     {
-        host : int
+        cls0 : int
         cls1 : int
         cls2 : int
         cls3 : int
         cls4 : int
         cls5 : int
         cls6 : int
-        lblh : string
+        lbl0 : string
         lbl1 : string
         lbl2 : string
         lbl3 : string
@@ -25,14 +25,14 @@ type Model =
 
 let initModel =
     {
-        host = 6
+        cls0 = 6
         cls1 = 4
         cls2 = 4
         cls3 = 6
         cls4 = 5
         cls5 = 6
         cls6 = 5
-        lblh = ""
+        lbl0 = ""
         lbl1 = ""
         lbl2 = ""
         lbl3 = ""
@@ -42,14 +42,14 @@ let initModel =
     }
 
 type Message =
-    | SetHost of int
+    | SetCls0 of int
     | SetCls1 of int
     | SetCls2 of int
     | SetCls3 of int
     | SetCls4 of int
     | SetCls5 of int
     | SetCls6 of int
-    | LblHost of string
+    | LblCls0 of string
     | LblCls1 of string
     | LblCls2 of string
     | LblCls3 of string
@@ -59,14 +59,14 @@ type Message =
 
 let update message model =
     match message with
-    | SetHost value -> { model with host = value }
+    | SetCls0 value -> { model with cls0 = value }
     | SetCls1 value -> { model with cls1 = value }
     | SetCls2 value -> { model with cls2 = value }
     | SetCls3 value -> { model with cls3 = value }
     | SetCls4 value -> { model with cls4 = value }
     | SetCls5 value -> { model with cls5 = value }
     | SetCls6 value -> { model with cls6 = value }
-    | LblHost value -> { model with lblh = value }
+    | LblCls0 value -> { model with lbl0 = value }
     | LblCls1 value -> { model with lbl1 = value }
     | LblCls2 value -> { model with lbl2 = value }
     | LblCls3 value -> { model with lbl3 = value }
@@ -74,13 +74,17 @@ let update message model =
     | LblCls5 value -> { model with lbl5 = value }
     | LblCls6 value -> { model with lbl6 = value }
 
+// Interface
 let view model dispatch =      
     concat {
+        // Header
         div{
             attr.``class`` "flex-container-1"
+            // Name and Logo
             div{
                 attr.``class`` "flex-container-2"
                 attr.id "header"
+                // Logo
                 a{
                     attr.href "https://github.com/vykrum/Hywe"
                     attr.target "blank"
@@ -90,10 +94,12 @@ let view model dispatch =
                         attr.src "https://vykrum.github.io/Hywe/favicon-32x32.png"
                     }
                 }
+                // Title
                 div{
                     attr.id "title"
                     " H Y W E"
                     }
+                // Acronym
                 div{
                     img{
                     attr.width "200"
@@ -102,6 +108,7 @@ let view model dispatch =
                     }
                 }
                 }
+            // Introduction
             div{
                 attr.id "description"
                 p{
@@ -111,32 +118,39 @@ let view model dispatch =
                 }
             }
         }
+        // Cluster Controls
         div{
+            // Cluster 0
             div{
                 attr.``class`` "flex-container-2"
+                // Slider 0
                 input{
                     attr.``class`` "slider"
                     attr.``type`` "range"
-                    attr.id "host"
+                    attr.id "cls0"
                     attr.min "6"
                     attr.max "25"
-                    bind.input.int model.host (fun a -> dispatch (SetHost a))
+                    bind.input.int model.cls0 (fun a -> dispatch (SetCls0 a))
                 }
+                // Cluster 0 Size Percentage
                 label{
                     attr.``class`` "label"
-                    attr.id "host"
+                    attr.id "cls0"
                     attr.``type`` "text"
-                    $" {model.host * 4}%%"
+                    $" {model.cls0 * 4}%%"
                 }
+                // Cluster 0 Name Input
                 input{
                     attr.``class`` "input"
                     attr.``type`` "text"
-                    attr.id "host"
-                    bind.input.string model.lblh (fun a -> dispatch (LblHost a))
+                    attr.id "cls0"
+                    bind.input.string model.lbl0 (fun a -> dispatch (LblCls0 a))
                 }
             }
-            div{
+            // Cluster 1
+            div{ 
                 attr.``class`` "flex-container-2"
+                // Slider 1
                 input{
                     attr.``class`` "slider"
                     attr.``type`` "range"
@@ -145,12 +159,14 @@ let view model dispatch =
                     attr.max "25"
                     bind.input.int model.cls1 (fun b -> dispatch (SetCls1 b))
                 }
+                // Cluster 1 Size Percentage
                 label{
                     attr.``class`` "label"
                     attr.id "cls1"
                     attr.``type`` "text"
                     $" {model.cls1 * 4}%%"
                 }
+                // Cluster 1 Name Input
                 input{
                     attr.``class`` "input"
                     attr.``type`` "text"
@@ -158,8 +174,10 @@ let view model dispatch =
                     bind.input.string model.lbl1 (fun a -> dispatch (LblCls1 a))
                 }
             }
+            // Cluster 2
             div{
                 attr.``class`` "flex-container-2"
+                // Slider 2
                 input{
                     attr.``class`` "slider"
                     attr.``type`` "range"
@@ -168,12 +186,14 @@ let view model dispatch =
                     attr.max "25"
                     bind.input.int model.cls2 (fun c -> dispatch (SetCls2 c))
                 }
+                // Cluster 2 Size Percentage
                 label{
                     attr.``class`` "label"
                     attr.id "cls2"
                     attr.``type`` "text"
                     $" {model.cls2 * 4}%%"
                 }
+                // Cluster 2 Name Input
                 input{
                     attr.``class`` "input"
                     attr.``type`` "text"
@@ -181,8 +201,10 @@ let view model dispatch =
                     bind.input.string model.lbl2 (fun a -> dispatch (LblCls2 a))
                 }
             }
+            // Cluster 3
             div{
                 attr.``class`` "flex-container-2"
+                // Slider 3
                 input{
                     attr.``class`` "slider"
                     attr.``type`` "range"
@@ -191,12 +213,14 @@ let view model dispatch =
                     attr.max "25"
                     bind.input.int model.cls3 (fun d -> dispatch (SetCls3 d))
                 }
+                // Cluster 3 Size Percentage
                 label{
                     attr.``class`` "label"
                     attr.id "cls3"
                     attr.``type`` "text"
                     $" {model.cls3 * 4}%%"
                 }
+                // Cluster 3 Name Input
                 input{
                     attr.``class`` "input"
                     attr.``type`` "text"
@@ -204,8 +228,10 @@ let view model dispatch =
                     bind.input.string model.lbl3 (fun a -> dispatch (LblCls3 a))
                 }
             }
+            // Cluster 4
             div{
                 attr.``class`` "flex-container-2"
+                // Slider 4
                 input{
                     attr.``class`` "slider"
                     attr.``type`` "range"
@@ -214,12 +240,14 @@ let view model dispatch =
                     attr.max "25"
                     bind.input.int model.cls4 (fun e -> dispatch (SetCls4 e))
                 }
+                // Cluster 4 Size Percentage
                 label{
                     attr.``class`` "label"
                     attr.id "cls4"
                     attr.``type`` "text"
                     $" {model.cls4 * 4}%%"
                 }
+                // Cluster 4 Name Input
                 input{
                     attr.``class`` "input"
                     attr.``type`` "text"
@@ -227,8 +255,10 @@ let view model dispatch =
                     bind.input.string model.lbl4 (fun a -> dispatch (LblCls4 a))
                 }
             }
+            // Cluster 5
             div{
                 attr.``class`` "flex-container-2"
+                // Slider 5
                 input{
                     attr.``class`` "slider"
                     attr.``type`` "range"
@@ -237,12 +267,14 @@ let view model dispatch =
                     attr.max "25"
                     bind.input.int model.cls5 (fun e -> dispatch (SetCls5 e))
                 }
+                // Cluster 5 Size Percentage
                 label{
                     attr.``class`` "label"
                     attr.id "cls5"
                     attr.``type`` "text"
                     $" {model.cls5 * 4}%%"
                 }
+                // Cluster 5 Name Input
                 input{
                     attr.``class`` "input"
                     attr.``type`` "text"
@@ -250,8 +282,10 @@ let view model dispatch =
                     bind.input.string model.lbl5 (fun a -> dispatch (LblCls5 a))
                 }
             }
+            // Cluster 6
             div{
                 attr.``class`` "flex-container-2"
+                // Slider 6
                 input{
                     attr.``class`` "slider"
                     attr.``type`` "range"
@@ -260,12 +294,14 @@ let view model dispatch =
                     attr.max "25"
                     bind.input.int model.cls6 (fun g -> dispatch (SetCls6 g))
                 }
+                // Cluster 6 Size Percentage
                 label{
                     attr.``class`` "label"
                     attr.id "cls6"
                     attr.``type`` "text"
                     $"{model.cls6 * 4}%%"
                 }
+                // Cluster 6 Name Input
                 input{
                     attr.``class`` "input"
                     attr.``type`` "text"
@@ -274,13 +310,22 @@ let view model dispatch =
                 }
             }   
             }
-        let (loc,wdt,hgt) = cls ([|model.host;model.cls1;model.cls2;model.cls3;model.cls4;model.cls5;model.cls6|])
-        let lbls = [|model.lblh;model.lbl1;model.lbl2;model.lbl3;model.lbl4;model.lbl5;model.lbl6|]
+
+        let (loc,wdt,hgt) = cls ([|model.cls0;model.cls1;model.cls2;model.cls3;model.cls4;model.cls5;model.cls6|])
+        
+        // Cluster Names
+        let lbls = [|model.lbl0;model.lbl1;model.lbl2;model.lbl3;model.lbl4;model.lbl5;model.lbl6|]
+        
+        // Cluster Colors
         let clrs = [|"#ececce";"#bccfd3";"#867869";"#687b7f";"#c6bdb4";"#3496a3";"#eabdb5"|]
+        
+        // The Clusters SVG
         div{
             attr.``class`` "center"
             cluster (Array.zip3 loc lbls clrs) wdt hgt
             }
+        
+        // Footer
         footer{
             a{
                 attr.id "footer"
@@ -298,6 +343,7 @@ let view model dispatch =
         }
     }
 
+// Bolero component handling state updates and rendering the user interface
 type MyApp() =
     inherit ProgramComponent<Model, Message>()
 
