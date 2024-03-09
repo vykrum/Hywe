cfa4a80721c329cb460d328905f28e2e08a9bf67
vykrum <testingwaters@gmail.com>
2024-03-09 11:50:09 +0530

    Shapes

---
+++ Shapes.fs
@@ -0,0 +1,29 @@
+﻿module Shapes
+
+open Hexel
+
+/// <summary> Ortogonal Hexel Sequence </summary>
+/// <param name="sqn"> Sequence to follow. </param>
+/// <param name="org"> Start Hexel. </param> 
+/// <param name="lgt"> Sequence Length. </param> 
+/// <param name="vrt"> Vertical / Horizontal. </param> 
+/// <returns> Array of sequential Hexels. </returns>
+let hxlOrt 
+    (sqn : Sqn)
+    (org : Hxl)
+    (lgt : int)
+    (vrt : bool) =
+    let hxx,hxy,hxz = org |> hxlVld sqn |> hxlCrd
+    match sqn with
+    | SQ11 | SQ12 | SQ13 | SQ14 | SQ15 | SQ16 | SQ17 | SQ18 | SQ19 | SQ20 | SQ21 | SQ22
+        -> match vrt with 
+            | true -> [|hxy..4..(hxy+lgt)|]
+                    |> Array.map (fun y -> [|RV(hxx,y,hxz);RV(hxx+1,y+2,hxz)|])
+                    |> Array.concat
+            | false -> Array.map (fun x -> RV (x,hxy,hxz)) [|hxx..2..(hxx+lgt)|]
+    | SQ23 | SQ24 | SQ25 | SQ26 | SQ27 | SQ28 | SQ29 | SQ30 | SQ31 | SQ32 | SQ33 | SQ34
+        -> match vrt with
+            | true -> Array.map (fun y -> RV (hxx,y,hxz)) [|hxy..2..(hxy+lgt)|]
+            | false -> [|hxx..4..(hxx+lgt)|] 
+                    |> Array.map (fun x -> [|RV(x,hxy,hxz);RV(x+2,hxy+1,hxz)|])
+                    |> Array.concat
