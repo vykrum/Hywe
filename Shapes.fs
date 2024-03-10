12712534186fbbe651e325cb997003f653493e4b
vykrum <testingwaters@gmail.com>
2024-03-10 16:52:45 +0530

    Shapes

--- Shapes.fs
+++ Shapes.fs
@@ -14,16 +14,21 @@ let hxlOrt
     (lgt : int)
     (vrt : bool) =
     let hxx,hxy,hxz = org |> hxlVld sqn |> hxlCrd
+    let lgt = lgt + (lgt%2)         
     match sqn with
     | SQ11 | SQ12 | SQ13 | SQ14 | SQ15 | SQ16 | SQ17 | SQ18 | SQ19 | SQ20 | SQ21 | SQ22
         -> match vrt with 
-            | true -> [|hxy..4..(hxy+lgt)|]
+            | true -> [|hxy..4..(hxy+lgt+4)|]
                     |> Array.map (fun y -> [|RV(hxx,y,hxz);RV(hxx+1,y+2,hxz)|])
                     |> Array.concat
-            | false -> Array.map (fun x -> RV (x,hxy,hxz)) [|hxx..2..(hxx+lgt)|]
+                    |> Array.take ((lgt/2)+1)
+            | false -> Array.map (fun x -> RV (x,hxy,hxz)) [|hxx..2..(hxx+lgt+4)|]
+                    |> Array.take ((lgt/2)+1)
     | SQ23 | SQ24 | SQ25 | SQ26 | SQ27 | SQ28 | SQ29 | SQ30 | SQ31 | SQ32 | SQ33 | SQ34
         -> match vrt with
             | true -> Array.map (fun y -> RV (hxx,y,hxz)) [|hxy..2..(hxy+lgt)|]
+                    |> Array.take ((lgt/2)+1)
             | false -> [|hxx..4..(hxx+lgt)|] 
                     |> Array.map (fun x -> [|RV(x,hxy,hxz);RV(x+2,hxy+1,hxz)|])
                     |> Array.concat
+                    |> Array.take ((lgt/2)+1)
