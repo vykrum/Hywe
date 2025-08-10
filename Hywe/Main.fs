2b8e2739cb236ab5b00ea3671808d01ce93ded4f
Vikram Subbaiah <vykrum@hotmail.com>
2025-08-10 22:38:18 +0530

    Boundary CSS

--- Hywe/Main.fs
+++ Hywe/Main.fs
@@ -283,14 +283,7 @@ let view model dispatch (js: IJSRuntime) =
             }
     }
 
-            (* div{
-                attr.``style`` "width: 100%;"
-                PolygonEditor.view model.PolygonEditor (PolygonEditorMsg >> dispatch) js
-
-                //Boundary.view model.boundary (BoundaryMsg >> dispatch)
-            }
-
-           // Download Button — placed *after* table and centered
+            (* // Download Button — placed *after* table and centered
             div {
                 attr.``style`` "width: 800px;"
                 div {
