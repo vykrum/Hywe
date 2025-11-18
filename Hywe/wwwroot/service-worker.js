5d026144bc70e05c0f39e7ddbd06801f3e63c9c2
Vikram Subbaiah <vykrum@hotmail.com>
2025-11-18 21:46:02 +0530

    Service Worker Updated

--- Hywe/wwwroot/service-worker.js
+++ Hywe/wwwroot/service-worker.js
@@ -1,20 +1,2 @@
-/// <reference lib="webworker" />
-
-declare const self: ServiceWorkerGlobalScope;
-export { };
-
-self.addEventListener("install", () => {
-    console.log("[SW] install");
-    self.skipWaiting();
-});
-
-self.addEventListener("activate", () => {
-    console.log("[SW] activate");
-    self.clients.claim();
-});
-
-// No caching – always hit the network
-self.addEventListener("fetch", (event) => {
-    // Allow the request to pass through untouched
-    return;
-});
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => self.clients.claim());
