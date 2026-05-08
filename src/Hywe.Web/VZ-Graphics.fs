447f3070dccac3b34292f851859b2074cb36f78f
vykrum <testingwaters@gmail.com>
2026-05-08 21:15:28 +0530

    Migrate 3D renderer from WebGL to WebGPU

--- src/Hywe.Web/VZ-Graphics.fs
+++ src/Hywe.Web/VZ-Graphics.fs
@@ -4,23 +4,6 @@ open Bolero
 open System.Collections.Frozen
 open Hywe.Core
 
-let vsSource = """
-    attribute vec3 a_position;
-    attribute vec4 a_color;
-    uniform mat4 u_projection;
-    uniform mat4 u_view;
-    varying vec4 v_color;
-    void main() {
-        gl_Position = u_projection * u_view * vec4(a_position, 1.0);
-        v_color = a_color;
-    }"""
-
-let fsSource = """
-    precision mediump float;
-    varying vec4 v_color;
-    void main() {
-        gl_FragColor = v_color;
-    }"""
 
 type hxgn = Template<
       """ <polygon 
